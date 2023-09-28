2fdb4093c505aecf5c9d9d07b112ecbfe2fa314d
biojuampa <104794253+biojuampa@users.noreply.github.com>
2023-09-28 15:24:11 -0300

    Laboratorio 03 terminado

---
+++ contracts/laboratorios/LAB03/NFTERC721.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
+contract NFTERC721 is ERC721, ERC721URIStorage {
+    constructor() ERC721("Juan Pablo", "JPNFT") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function transFer(address from, address to,uint256 tokenId) public {
+        _transfer(from, to, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
